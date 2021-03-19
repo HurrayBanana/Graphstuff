4112fb6ca3725805a8b818dd2d32df2637bed07b
Hurray Banana <HurrayBanana@users.noreply.github.com>
2021-03-19 11:45:44 +0000

    Add project files.

---
+++ Graphstuff/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphstuff
+{
+    class Animal
+    {
+        private float weight;
+        private string name;
+        private DateTime born;
+
+        public float Weight
+        {
+            get { return weight; }
+            set { 
+                    if (weight > 0)
+                        weight = value; 
+                }
+        }
+
+        public Animal(string name, float weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+}
---
+++ Graphstuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graphstuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
