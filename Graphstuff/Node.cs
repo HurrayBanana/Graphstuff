aa640436bcbecdbf7156659b767f28d415de1bce
Hurray Banana <HurrayBanana@users.noreply.github.com>
2021-03-19 12:56:41 +0000

    Started implementing Node and TreeNode

---
+++ Graphstuff/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphstuff
+{
+    class Node
+    {
+        private object data;
+        private List<Node> neighbours = new List<Node>();
+        public List<Node> Neighbours
+        {
+            get { return neighbours; }
+        }
+        public Node(object data)
+        {
+            this.data = data;
+        }
+        public object Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public void AddNeighbour(Node node)
+        {
+            neighbours.Add(node);
+        }
+    }
+}
---
+++ Graphstuff/TreeNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphstuff
+{
+    class TreeNode : Node
+    {
+        const int LEFT = 0;
+        const int RIGHT = 1;
+        public TreeNode(TreeNode node)
+            :base(node)
+        {
+        }
+        public TreeNode Left
+        {
+            get {
+                if (Neighbours.Count == 0)
+                    return null;
+                else 
+                    return (TreeNode)Neighbours[LEFT]; }
+            set {
+                if (Neighbours.Count )
+                Neighbours[LEFT] = value;}
+        }
+        public TreeNode Right
+        {
+            get { return (TreeNode)Neighbours[RIGHT]; }
+            set { Neighbours[RIGHT] = value; }
+        }
+    }
+}
